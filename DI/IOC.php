8101b0077557c28c0badb3c5df9aabfebce4cbd2
jackwu <403226004@qq.com>
2020-03-04 12:08:50 +0800

    reset test step2

--- DI/IOC.php
+++ DI/IOC.php
@@ -2,4 +2,14 @@
 class IOC
 {
     protected $bindArray=[];
+    public function binds($abstract,$class)
+    {
+        $this->bindArray[$abstract]["class"]=function ($ioc) use ($class) {
+            return $ioc->build($class);
+        };
+    }
+    public function build($class)
+    {
+
+    }
 }
